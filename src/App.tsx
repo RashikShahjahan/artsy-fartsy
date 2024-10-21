3cdcf2974fa5c987231514d9732c9c6eb117e9da
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2024-10-21 16:34:15 -0400

    handles code with mutiple lines

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Canvas } from '@react-three/fiber'
 import Line from './Line';
 import Arc from './Arc';
-import './App.css';
 import { useState } from 'react';
 import { Command, interpret } from './interpret';
 /*Tasks:
@@ -10,10 +9,12 @@ Setup react-three/fiber with react [Done]
     -Add textbox in react to call interpreter on button click
     -Create render function to render the output list of objects
 
-Write version of interpreter to run single command->Line and arc:
+Write version of interpreter to run single command->Line and arc:[DONE]
     -Create lexer for ARC and LINE
     -Parse tokens to create ARC and LINE objects
     -return ARC and LINE objects
+
+Fix tailwind
 
 Enhancements:
     Add variables and expressions
@@ -28,14 +29,15 @@ function App() {
   const [drawCommands, setDrawCommands] = useState<Command[]>([]);
   
   function submitInput() {
-    console.log(input);
-    setDrawCommands(interpret(input));
+    const commands = interpret(input);
+    console.log(commands)
+    setDrawCommands(commands);
   }
 
   return (
-    <div className="flex w-full h-screen">
-      <div className="w-1/2 h-full">
-        <Canvas className="w-full h-full">
+    <div >
+      <div >
+        <Canvas >
           {drawCommands.map((command, index) => (
             command.type === 'line' ? 
               <Line key={index} start={[command.args[0], command.args[1], 0]} end={[command.args[2], command.args[3], 0]} /> :
@@ -46,19 +48,19 @@ function App() {
                 end={[command.args[4], command.args[5], 0]}
                 startAngle={command.args[6]}
                 endAngle={command.args[7]}
-                clockwise={command.args[11] as boolean}
-                rotation={command.args[12]}
+                clockwise={command.args[8] as boolean}
+                rotation={command.args[9]}
               />
           ))}
         </Canvas>
       </div>
-      <div className="w-1/2 h-full flex flex-col justify-center items-center">
-        <input 
-          type="text" 
+      <div>
+        <textarea 
           value={input} 
           onChange={(e) => setInput(e.target.value)} 
           placeholder="Enter drawing commands"
           className="w-4/5 mb-4 p-2 border border-gray-300 rounded"
+          rows={5}
         />
         <button onClick={() => submitInput()} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
           Draw
