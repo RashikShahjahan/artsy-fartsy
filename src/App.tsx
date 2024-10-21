977ca725d3b641555da2ebfe820cd6119c520e08
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2024-10-21 14:33:33 -0400

    made same changes for ARC

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Canvas } from '@react-three/fiber'
 import Line from './Line';
+import Arc from './Arc';
 import './App.css';
 import { useState } from 'react';
 /*Tasks:
-Setup react-three/fiber with react 
+Setup react-three/fiber with react [Done]
     -Create component for ARC and LINE
     -Add textbox in react to call interpreter on button click
     -Create render function to render the output list of objects
@@ -24,6 +25,11 @@ type DrawCommand = {
   args: {
     start: [number, number, number];
     end: [number, number, number];
+    center?: [number, number, number];
+    startAngle?: number;
+    endAngle?: number;
+    clockwise?: boolean;
+    rotation?: number;
   };
 }
 
@@ -35,7 +41,7 @@ function App() {
     console.log(input);
     setDrawCommands([
       { type: 'line', args: { start: [0, 0, 0], end: [1, 1, 1] } },
-      { type: 'line', args: { start: [0, 2, 0], end: [4, 1, 1] } },
+      { type: 'arc', args: { start: [0, 2, 0], end: [4, 1, 1], center: [2, 1.5, 0.5], startAngle: 0, endAngle: Math.PI, clockwise: true, rotation: 0 } },
     ]);
   }
 
@@ -43,7 +49,18 @@ function App() {
     <div>
       <Canvas>
         {drawCommands.map((command, index) => (
-          <Line key={index} start={command.args.start} end={command.args.end} />
+          command.type === 'line' ? 
+            <Line key={index} start={command.args.start} end={command.args.end} /> :
+            <Arc 
+              key={index} 
+              start={command.args.start} 
+              end={command.args.end} 
+              center={command.args.center!} 
+              startAngle={command.args.startAngle!} 
+              endAngle={command.args.endAngle!} 
+              clockwise={command.args.clockwise!} 
+              rotation={command.args.rotation!} 
+            />
         ))}
       </Canvas>
       <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
