d811ed2f70e74408efbf7f03a63fbe328847e6c9
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2024-10-22 14:59:29 -0400

    notes

--- backend/index.ts
+++ backend/index.ts
@@ -1,21 +1,28 @@
 /*Tasks:
-Saving Drawings
-    -Create button to save drawings
-    -Save drawings to Database
-    -Load drawings from Database
+Authentication
+  - Clerk middleware to protect routes
+  - Add clerk auth logic to the endpoints
 
-Routing and Authentication
-    -Create login/register
-    -Create protected routes
-    -Create logout
+Database
+  - Setup Prisma and connect to PostgreSQL
+  - Create prisma schema to store images and user 
+ 
+Endpoints
+  - Create endpoint to manage login, signup, and logout
+  - POST endpoint save_art
+  - GET endpoint retrieve_gallery
+  - POST endpoint like_art
+  - POST endpoint unlike_art
+  - endpoint to make LLM call
+  - GET endpoint retrieve_user_arts(Optional)
+  - GET endpoint retrieve_user_followers(Optional)
+  - GET endpoint retrieve_user_following(Optional)
+  - POST endpoint follow_user(Optional)
+  - POST endpoint unfollow_user(Optional)   
 
-News Feed which lets you see all drawings
-
-Enhancements:
-    Add coloring[Done]
-    Add variables and expressions
-    Add loops
-    Add error validation
+Deployment:
+  - Dockerize the app(Optional)
+  - Deploy the app 
 */
 
 import express from 'express';
@@ -25,6 +32,7 @@ import { interpret } from './interpret';
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 app.post('/interpret', (req, res) => {
     console.log(req.body);
     const { code } = req.body;
